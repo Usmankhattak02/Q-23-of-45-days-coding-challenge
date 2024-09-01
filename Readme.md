c8a12fd588014b6281fb38e36a2874692c234ad3
Muhammad Usman <usmanlive399@gmail.com>
2024-09-01 12:50:15 +0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Q-23 of 45 days codding challenge
+# q-23 ts/js
---
+++ main.js
@@ -0,0 +1,31 @@
+"use strict";
+// Question 23
+// Conditional Tests: Write a series of conditional tests.Predict the results of each test.
+let book = `English`;
+console.log(" book == `English`? I Predict True.");
+console.log(book == `English`); // true
+console.log("book == `Urdu`? I Predict False.");
+console.log(book == `Urdu`); //False
+// Example 2
+let car = `Civic`;
+console.log(" car == `Civic`? so its True.");
+console.log(car == `Civic`); //true
+console.log("car ==`Toyota`? so its False.");
+console.log(car == `Toyota`); //false
+//Example 3 
+let languages = ("Urdu , Arabic");
+console.log("languages == `Urdu , Arabic`? so its true.");
+console.log(languages == `Urdu , Arabic`); //true
+console.log("languages == `Sindhi , Farsi`? so its true.");
+console.log(languages == `Sindhi , Farsi`); // false
+// Example 4
+let myname = ("Arain");
+let myname2 = ("Aryan");
+console.log("myName == `Arain`? its Predict True.");
+console.log(myname == `Arain`); //True
+console.log("myName2 ==`Aryan`? its Predict True");
+console.log(myname2 == `Aryan`); //True
+console.log("myName ==`Alian`? its Predict False.");
+console.log(myname == `Alian`); //false
+console.log("myName2 == `Alayin`? its Predict False.");
+console.log(myname2 == `Alayin`); //false
